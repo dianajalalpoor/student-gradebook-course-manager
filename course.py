49ca379a456218857401e6919e0c1146d3b40c0f
dianajalalpoor <dianajalalpoor19@gmail.com>
2026-07-02 19:47:01 +0430

    Create course.py

---
+++ course.py
@@ -0,0 +1,42 @@
+class Course:
+    def __init__(self, course_code, course_name):
+        self.__course_code = course_code
+        self.__course_name = course_name
+        self.__students = []
+        self.__assessments = []
+
+
+    def get_course_code(self):
+        return self.__course_code
+    
+
+    def get_course_name(self):
+        return self.__course_name
+    
+
+    def add_student(self, student_id):
+        if student_id not in self.__students:
+            self.__students.append(student_id)
+
+
+    def add_assessment(self, assessment):
+        self.__assessments.append(assessment)
+
+
+    def find_assessment(self, title):
+        for assessment in self.__assessments:
+            if assessment.title == title:
+                return assessment
+        
+        print("Not found")
+        return None
+
+
+    def display_info(self):
+        print("Course code:", self.__course_code)
+        print("course name:", self.__course_name)
+        print("Enrolled student:", len(self.__students))
+        print("assessments:")
+        for assessment in self.__assessments:
+            print("-", assessment.title, "/ Max Score:", assessment.max_score)
+
